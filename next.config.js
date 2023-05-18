85cb8b3843a7bdfc6d261d30a4f9412cac5e4e6e
Julio Ferreira <julio.x.ferriera@questdiagnostics.com>
2023-05-18 15:48:01 -0300

    Add configurations to deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: {
+        loader: "imgix",
+        path: 'the "domain" of your Imigix source',
+    },
 };
