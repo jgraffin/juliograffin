364733df87bd6e75863a83c0bde20322b42b3b00
Julio Ferreira <julio.x.ferriera@questdiagnostics.com>
2023-05-24 07:55:26 -0300

    Add configurations

--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
-/** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 module.exports = {
-    reactStrictMode: false,
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-    images: {
-        loader: "imgix",
-        path: "https://juliograffin.imgix.net",
-    },
+    basePath: '',
 };
